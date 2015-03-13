93bb0b0be27d6347443301902388c61dc7723c6d
abcl0653 <fd.cailin@gmail.com>
2015-03-13 14:19:39 +0800

    Duck initial

---
+++ src/Duck.java
@@ -0,0 +1,16 @@
+
+public abstract class Duck {
+	FlyBehavior flyBehavior;
+	QuackBehavior quackBehavior;
+	public Duck(){};
+	
+	public void performFly(){
+		flyBehavior.fly();
+	};
+	
+	public void performQuack(){
+		quackBehavior.quack();
+	};
+	
+	public abstract void display();
+}
---
+++ src/MiniDuckSimulator.java
@@ -0,0 +1,12 @@
+
+public class MiniDuckSimulator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Duck mallardDuck = new MallardDuck();
+		mallardDuck.display();
+		mallardDuck.performFly();
+		mallardDuck.performQuack();
+	}
+
+}
